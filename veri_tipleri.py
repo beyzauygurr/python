489e2d6cb37a72164288d8b166dd211223f51e88
beyzauygurr <101145504+beyzauygurr@users.noreply.github.com>
2024-05-06 00:34:38 +0300

    Add files via upload

---
+++ veri_tipleri.py
@@ -0,0 +1,39 @@
+# String (metin) veri tipi
+string_degisken = "Merhaba, Dünya!"
+
+# Tam sayı (integer) veri tipi
+tam_sayi_degisken = 42
+
+# Ondalıklı sayı (float) veri tipi
+ondalikli_sayi_degisken = 3.14
+
+# Boole (Boolean) veri tipi
+dogru_degisken = True
+yanlis_degisken = False
+
+# Liste (list) veri tipi
+liste_degisken = [1, 2, 3, 4, 5]
+
+# Demet (tuple) veri tipi
+demet_degisken = (1, 2, 3, 4, 5)
+
+# Sözlük (dictionary) veri tipi
+sozluk_degisken = {"anahtar1": "değer1", "anahtar2": "değer2"}
+
+# Kümeler (set) veri tipi
+kume_degisken = {1, 2, 3, 4, 5}
+
+# None (Boş) veri tipi
+bos_degisken = None
+
+# Veri tiplerini ve değerlerini ekrana yazdırma
+print("String Değişken:", string_degisken)
+print("Tam Sayı Değişken:", tam_sayi_degisken)
+print("Ondalıklı Sayı Değişken:", ondalikli_sayi_degisken)
+print("Doğru Değişken:", dogru_degisken)
+print("Yanlış Değişken:", yanlis_degisken)
+print("Liste Değişken:", liste_degisken)
+print("Demet Değişken:", demet_degisken)
+print("Sözlük Değişken:", sozluk_degisken)
+print("Küme Değişken:", kume_degisken)
+print("Boş Değişken:", bos_degisken)
